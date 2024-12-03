2d99b5f73c56618cfd73e7443aa7044e85be37bd
hoigoysohio <njuli@yahoo.com>
2024-12-03 17:37:21 +0000

    Done

---
+++ main.py
@@ -0,0 +1,24 @@
+a = -342
+a = bool(a)
+print(a)
+print(type(a))
+
+b = 32.09
+b = int(b)
+print(b)
+print(type(b))
+
+c = "12"
+c = float(c)
+print(c)
+print(type(c))
+
+d = 99
+d = str(d)
+print(d)
+print(type(d))
+
+
+
+
+
